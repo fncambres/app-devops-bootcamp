790ad06d7f6775743a6e76fa0d6ff5dd4424410c
Ezequiel Gonzalez <edgaregonzalez@gmail.com>
2023-03-17 00:06:25 -0300

    fix test components

--- api-store/src/app.controller.spec.ts
+++ api-store/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "DevOps Bootcamp!"', () => {
+      expect(appController.getHello()).toBe('DevOps Bootcamp!');
     });
   });
 });
